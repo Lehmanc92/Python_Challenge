4a9f37fbc72e97ba2628df3029c0855c24b88fe4
Christian L <Lehmanc92@gmail.com>
2021-04-09 10:12:59 -0400

    PyPoll with candidate vote counter added

--- PyPoll/Main.py
+++ PyPoll/Main.py
@@ -34,17 +34,30 @@ with open(csvpath) as csv_file:
 
 total_votes = 0
 candidate_list = []
+candidate_votes = []
 candidate_totals = {}
 
-# Create candidate list
+# Create candidate list by finding all unique candidates in data
 
 for row in data_csv:
+
     if row[2] not in candidate_list:
         candidate_list.append(row[2])
+        candidate_totals[row[2]] = 0
+        candidate_votes.append(1)
+    
+    #Tally votes for each candidate
+    
+    else:
+        candidate_votes[candidate_list.index(row[2])] += 1
 
 # for candidate in candidate_list:
-#     candidate_totals.update(candidate)
+#     candidate_totals[candidate] = 0
+
+# for row in data_csv:
+#     if 
 
 
 print(candidate_list)
+print(candidate_votes)
 print(candidate_totals)
